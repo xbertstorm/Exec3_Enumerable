5faeddc1bdf65e36da93eef98ce676e98d7649d9
xbertstorm5566 <xbertstorm5566@gmail.com>
2022-11-07 16:52:38 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> number = Enumerable.Range(1, 10);
+
+			foreach (var x in number)
+			{
+				Console.WriteLine(x);
+			}
+		}
+	}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<int> number = Enumerable.Range(10, 6);
+
+			foreach (var x in number)
+			{
+				Console.WriteLine(x);
+			}
+		}
+	}
+}
